9655778c42ad60b8c6289ef80dd8816511de5c2c
NotYourAbu <68179601+SyntaxErrorSolos@users.noreply.github.com>
2023-06-22 18:52:54 +0500

    Adding a /fact endpoint

--- index.js
+++ index.js
@@ -2,13 +2,13 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-app.listen(process.env.PORT || 8080)
+app.listen(process.env.PORT || 8080);
 app.use(cors());
-console.log("App started")
+console.log("App started");
 
 // <<--------- BASIC ---------->>
 app.get("/", (req, res) => {
-  res.json({ error: "Send a GET request to /quote" });
+  res.json({ error: "Available endpoints: /quote and /fact (GET)" });
 });
 
 // <<--------- QUOTES ---------->>
@@ -17,7 +17,14 @@ app.get("/quote", (req, res) => {
   const quotes_length = quotes.length;
   const randomizer = Math.floor(Math.random() * quotes_length);
   const random_quote = quotes[randomizer];
-  res.json({ quote: random_quote });
+  res.json({ quote: random_quote, Warning: "Quote may be incorrect" });
 });
 
-
+// <<--------- FACTS ---------->>
+app.get("/fact", (req, res) => {
+  const facts = require("./facts.json");
+  const facts_length = facts.length;
+  const randomizer = Math.floor(Math.random() * facts_length);
+  const random_fact = facts[randomizer];
+  res.json({ fact: random_fact, Warning: "Facts may be incorrect" });
+});
